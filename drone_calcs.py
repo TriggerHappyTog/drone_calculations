9303646bfa69df3de5a8f023fe4c4587b26cfe7d
TriggerHappyTog <flamingjackpot@gmail.com>
2017-04-28 15:17:06 +0200

    Remove input commands so that they do not appear when running GUI. GUI now functional

--- drone_calcs.py
+++ drone_calcs.py
@@ -1,6 +1,3 @@
-import sys
-
-
 def pres_alt_calc(qnh, air_elev):
     if qnh >= 1013.25:
         qnh = int(qnh) - 1013.25
@@ -29,39 +26,3 @@ def den_alt_calc(temp, qnh, air_elev):
     answer = original_pres_alt + temp
 
     return answer
-
-
-print("What would you like to calculate? A = pressure altitude. B = Density Altitude.")
-choice = str(sys.stdin.readline()).strip()
-choice = str(choice.lower()).strip()
-
-while choice != 'a' and choice != 'b':
-    print('That aint a choice partner, try again.')
-    choice = str(sys.stdin.readline()).strip().lower()
-
-if str(choice) == "a":
-        print("What's your QNH chief?")
-        qnh = int(sys.stdin.readline())
-
-        print("What's your airfield elevation (in feet)?")
-        air_elev = int(sys.stdin.readline())
-
-        answer = pres_alt_calc(qnh, air_elev)
-        print('Your pressure altitutde is %d feet, bub.' % answer)
-
-elif str(choice) == "b":
-        # print("What is your pressure altitude chief?")
-        # pres_alt = int(sys.stdin.readline().strip())
-
-        print("What's your QNH chief?")
-        qnh = int(sys.stdin.readline())
-
-        print("What's your airfield elevation (in feet)?")
-        air_elev = int(sys.stdin.readline())
-
-        print("What's your temp?")
-        temp = int(sys.stdin.readline().strip())
-
-        answer = den_alt_calc(temp, qnh, air_elev)
-
-        print('Your density altitude is %d feet, bub.' % answer)
--- drone_gui.py
+++ drone_gui.py
@@ -1,4 +1,4 @@
-from Tkinter import Tk, Label, Button
+from Tkinter import Tk, Label, Button, Entry, IntVar, W, E
 import drone_calcs
 
 
@@ -11,6 +11,25 @@ class DroneGUI:
         self.label = Label(master, text="Useful little tool for calculating pressure and density.")
         self.label.pack()
 
+        self.label = Label(master, text="Temp")
+        self.label.pack()
+
+        vcmd = master.register(self.validate)  # we have to wrap the command
+        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P', "temp"))
+        self.entry.pack()
+
+        self.label = Label(master, text="QNH")
+        self.label.pack()
+
+        self.entry2 = Entry(master, validate="key", validatecommand=(vcmd, '%P', "qnh"))
+        self.entry2.pack()
+
+        self.label = Label(master, text="Air Elevation")
+        self.label.pack()
+
+        self.entry3 = Entry(master, validate="key", validatecommand=(vcmd, '%P', "air_elev"))
+        self.entry3.pack()
+
         self.pressure_button = Button(master, text="Pressure", command=self.pressure)
         self.pressure_button.pack()
 
@@ -20,18 +39,47 @@ class DroneGUI:
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
 
+        self.total = 0
+        self.qnh = 0
+        self.air_elev = 0
+        self.temp = 0
+
+        self.label2 = Label(master, text="Answer:")
+        self.label2.pack()
+
+        self.total_label_text = IntVar()
+        self.total_label_text.set(self.total)
+        self.total_label = Label(master, textvariable=self.total_label_text)
+        self.total_label.pack()
+
     def pressure(self):
-        qnh = 1027
-        air_elev = 4400
-        result = drone_calcs.pres_alt_calc(qnh, air_elev)
-        print(result)
+        result = drone_calcs.pres_alt_calc(self.qnh, self.air_elev)
+        self.total_label_text.set(result)
 
     def density(self):
-        temp = 25
-        qnh = 1027
-        air_elev = 4400
-        result = drone_calcs.den_alt_calc(temp, qnh, air_elev)
-        print(result)
+        result = drone_calcs.den_alt_calc(self.temp, self.qnh, self.air_elev)
+        self.total_label_text.set(result)
+
+    def validate(self, new_text, data_type):
+        if not new_text:
+            if data_type == "temp":
+                self.temp = 0
+            elif data_type == "qnh":
+                self.qnh = 0
+            elif data_type == "air_elev":
+                self.air_elev = 0
+            return True
+
+        try:
+            if data_type == "temp":
+                self.temp = int(new_text)
+            elif data_type == "qnh":
+                self.qnh = int(new_text)
+            elif data_type == "air_elev":
+                self.air_elev = int(new_text)
+            return True
+        except ValueError:
+            return False
 
 
 root = Tk()
